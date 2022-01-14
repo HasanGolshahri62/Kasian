2c5661f26a3f1dfba22082fe867dbad331bad7e0
HasanGolshahri62 <72038685+HasanGolshahri62@users.noreply.github.com>
2022-01-14 10:42:03 -0800

    Update define-regard.module.ts

--- src/app/General/BasicInfo/define-regard/define-regard.module.ts
+++ src/app/General/BasicInfo/define-regard/define-regard.module.ts
@@ -7,19 +7,21 @@ import { IonicModule } from '@ionic/angular';
 import { DefineRegardPageRoutingModule } from './define-regard-routing.module';
 
 import { DefineRegardPage } from './define-regard.page';
-import { FormToolbarComponent } from '../../../GeneralComponent/form-toolbar/form-toolbar.component';
-import { GridViewComponent } from '../../../GeneralComponent/grid-view/grid-view.component';
-import { BorderControlDirective } from '../../../TotalDirectives/GraphicalDirectives/border-control.directive';
 import { CrudManagerService } from '../../../TotalServices/ManageCrud/crud-manager.service';
+import { FormToolbarModule } from '../../../GeneralComponent/form-toolbar/form-toolbar.module';
+import { GridViewComponentModule } from '../../../GeneralComponent/grid-view/grid-view.module';
+import { BorderControlModule } from '../../../TotalDirectives/GraphicalDirectives/border-control.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    FormToolbarModule,GridViewComponentModule,BorderControlModule,
     DefineRegardPageRoutingModule
   ],
-  declarations: [DefineRegardPage,FormToolbarComponent,GridViewComponent, BorderControlDirective],
+  declarations: [DefineRegardPage],
   providers:[CrudManagerService]
 })
 export class DefineRegardPageModule {}
+
