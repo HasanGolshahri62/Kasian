a49e66a993aba2308b969baf43701c5dccb8601d
HasanGolshahri62 <72038685+HasanGolshahri62@users.noreply.github.com>
2022-01-14 11:07:02 -0800

    Add files via upload

--- src/app/Treasury/Operators/receipt/receipt.module.ts
+++ src/app/Treasury/Operators/receipt/receipt.module.ts
@@ -7,20 +7,21 @@ import { IonicModule } from '@ionic/angular';
 import { ReceiptPageRoutingModule } from './receipt-routing.module';
 
 import { ReceiptPage } from './receipt.page';
-import { FormToolbarComponent } from '../../../GeneralComponent/form-toolbar/form-toolbar.component';
-import { GridViewComponent } from '../../../GeneralComponent/grid-view/grid-view.component';
-import { BorderControlDirective } from '../../../TotalDirectives/GraphicalDirectives/border-control.directive';
-import { LovComponent } from '../../../GeneralComponent/lov/lov.component';
 import { CrudManagerService } from '../../../TotalServices/ManageCrud/crud-manager.service';
+import { FormToolbarModule } from '../../../GeneralComponent/form-toolbar/form-toolbar.module';
+import { GridViewComponentModule } from '../../../GeneralComponent/grid-view/grid-view.module';
+import { LovModule } from '../../../GeneralComponent/lov/lov.module';
+import { BorderControlModule } from '../../../TotalDirectives/GraphicalDirectives/border-control.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    FormToolbarModule,GridViewComponentModule,LovModule,BorderControlModule,
     ReceiptPageRoutingModule
   ],
-  declarations: [ReceiptPage,FormToolbarComponent,GridViewComponent, BorderControlDirective,LovComponent],
+  declarations: [ReceiptPage],
   providers:[CrudManagerService]
 })
 export class ReceiptPageModule {}
