1596f6c0cbbcf4b1fde530ef1f62d7c4c4b23906
HasanGolshahri62 <72038685+HasanGolshahri62@users.noreply.github.com>
2022-01-14 10:57:03 -0800

    Update define-bank.module.ts

--- src/app/Treasury/BasicInfo/define-bank/define-bank.module.ts
+++ src/app/Treasury/BasicInfo/define-bank/define-bank.module.ts
@@ -7,19 +7,20 @@ import { IonicModule } from '@ionic/angular';
 import { DefineBankPageRoutingModule } from './define-bank-routing.module';
 
 import { DefineBankPage } from './define-bank.page';
-import { FormToolbarComponent } from '../../../GeneralComponent/form-toolbar/form-toolbar.component';
-import { GridViewComponent } from '../../../GeneralComponent/grid-view/grid-view.component';
-import { BorderControlDirective } from '../../../TotalDirectives/GraphicalDirectives/border-control.directive';
 import { CrudManagerService } from '../../../TotalServices/ManageCrud/crud-manager.service';
+import { FormToolbarModule } from '../../../GeneralComponent/form-toolbar/form-toolbar.module';
+import { GridViewComponentModule } from '../../../GeneralComponent/grid-view/grid-view.module';
+import { BorderControlModule } from '../../../TotalDirectives/GraphicalDirectives/border-control.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    FormToolbarModule,GridViewComponentModule,BorderControlModule,
     DefineBankPageRoutingModule
   ],
-  declarations: [DefineBankPage,FormToolbarComponent,GridViewComponent, BorderControlDirective],
+  declarations: [DefineBankPage],
   providers:[CrudManagerService]
 })
 export class DefineBankPageModule {}
